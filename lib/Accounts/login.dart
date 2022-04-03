794d63b119132a18a7b4f691ecbebc0978b349dc
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-03 14:45:20 +0300

    created a clipper for a container in the login page

--- lib/Accounts/login.dart
+++ lib/Accounts/login.dart
@@ -12,7 +12,34 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        child: child));
+        body: SafeArea(
+            child: ClipPath(
+      clipper: MyClipper(),
+      child: Container(
+          height: size.height * 0.3,
+          width: size.width,
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 41, 5, 18),
+            shape: BoxShape.rectangle,
+          )),
+    )));
   }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    var controllPoint = Offset(50, size.height);
+    var endPoint = Offset(size.width / 2, size.height);
+    path.quadraticBezierTo(
+        controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
