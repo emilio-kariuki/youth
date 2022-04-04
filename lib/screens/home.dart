b0fe23d39252d2122d18a7fa5d55efe3f3b2f74f
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-04 10:22:40 +0300

    April 4, 2022 10:22 AM

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,25 +13,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 21, 20, 27),
-        title: Text("Youth Parish",style: GoogleFonts.roboto(fontSize: 20,fontWeight: FontWeight.w500)),
-        elevation: 0,
-
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: size.height * 0.15,
-          width: size.width,
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: Colors. blueGrey,
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(width: 1, color: const Color.fromARGB(255, 8, 9, 14))
-          ),
-        ),
-      ),
+      
       );
   }
 }
